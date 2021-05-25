5448e7b156fe1dcaa5f29e3d7c8bfa8c8a94eb33
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-24 22:29:11 -0500

    Correction for morgo config

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-#app.config["MONGO_URI"] = os.environ.get('MONGO_URI', '')
-app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI', '')
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
 mongo = PyMongo(app)
 
 @app.route("/")
